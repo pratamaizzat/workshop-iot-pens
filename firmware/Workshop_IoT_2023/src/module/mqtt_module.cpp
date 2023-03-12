5e5e3c241bf65e23c64a1de4fe010cc4ccb36844
Nanda Rizky Oktapradana <56052061+Rizky2110@users.noreply.github.com>
2023-03-13 00:15:06 +0700

    make file platform io for firmware and make mqtt_module.h and mqtt_module.cpp for function mqtt

---
+++ firmware/Workshop_IoT_2023/src/module/mqtt_module.cpp
@@ -0,0 +1,114 @@
+#include "mqtt_module.h"
+
+MQTTClient client;
+
+MQTTClient client(2300);
+#define MSG_BUFFER_SIZE_PUB (2300)
+#define VAR_BUFFER_SIZE (50)
+char subscriber[VAR_BUFFER_SIZE];
+String mqtt_test = "Not Connected";
+
+static char msg_pub[MSG_BUFFER_SIZE_PUB];
+char data_pub[MSG_BUFFER_SIZE_PUB];
+
+void message_received(String &topic, String &payload)
+{
+    JSONVar message = JSON.parse(payload);
+    if (topic == subscriber)
+    {
+        if (message.hasOwnProperty("action"))
+        {
+        }
+    }
+}
+
+class iMQTT
+{
+
+public:
+    iMQTT() {}
+
+    void setup(const char *server, Client &connection)
+    {
+        Serial.println("MQTT Setup");
+        client.begin(server, connection);
+        client.onMessage(message_received);
+    }
+
+    void connect(const char *sub1)
+    {
+        // generate random id
+        String client_id = "BreakerClient-";
+        client_id += String(random(0xffff), HEX);
+
+        // try connect to mqtt broker
+        Serial.print("\nMQTT Connecting");
+        int timeout = 0;
+        while (!client.connect(client_id.c_str(), "admin01", "zJJ9M7sKCW"))
+        {
+            Serial.print(".");
+            // timeout for break loop
+            timeout++;
+
+            delay(100);
+
+            if (timeout > 3)
+            {
+                break;
+            }
+        }
+        if (check() == true)
+        {
+            mqtt_test = "Connected";
+            Serial.println("\nMQTT Connected!");
+        }
+        else
+        {
+            mqtt_test = "Not Connected";
+            // hardwares.led_red();
+            Serial.println("\nMQTT Not Connected!");
+        }
+
+        subscribe(sub1);
+    }
+
+    void subscribe(const char *subs)
+    {
+        client.subscribe(subs);
+    }
+
+    bool publish(const char *topic, String payload)
+    {
+        memset(data_pub, 0, sizeof(data_pub));
+        memset(msg_pub, 0, sizeof(msg_pub));
+        // delay(5);
+
+        payload.toCharArray(data_pub, (payload.length() + 1));
+        snprintf(msg_pub, MSG_BUFFER_SIZE_PUB, "%s", data_pub);
+        delay(1);
+        return client.publish(topic, msg_pub, true, 0); // max length message 1420
+    }
+
+    void loop(const char *sub1)
+    {
+        client.loop();
+        // Serial.print("MQTT Connection - ");
+        // Serial.println(client.connected());
+        if (!client.connected())
+        {
+            connect(sub1);
+        }
+    }
+
+    bool check()
+    {
+        return client.connected();
+    }
+
+    void convert_string_to_char(String mess, char *variable)
+    {
+        static char dt[VAR_BUFFER_SIZE];
+        mess.toCharArray(dt, (mess.length() + 1));
+        snprintf(variable, VAR_BUFFER_SIZE, "%s", dt);
+    }
+};
---
+++ firmware/Workshop_IoT_2023/src/module/mqtt_module.h
@@ -0,0 +1,9 @@
+#ifndef mqtt_module_h
+#define mqtt_module_h
+
+#include "MQTTClient.h"
+#include "mqtt_module.h"
+#include "MQTT.h"
+#include <Arduino_JSON.h>
+
+#endif
